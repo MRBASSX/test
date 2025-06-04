3cc5ccb1d6f93e1be764c6e5d2d41f9502613e51
mrbassx <abassiddrisu9222@gmail.com>
2025-06-04 11:21:31 +0000

    up

---
+++ Software Weekdays/js/main.js
@@ -0,0 +1,65 @@
+// let  A = "Orange juice";
+// const B = "Water";
+// var C =  "Pineaple juice";
+
+// console.log(C +" "+ A+"                          "+ B)
+
+
+// // This 
+let arrayOfData = [
+    {
+        name:"Mark",
+        age:100
+    },
+     {
+        name:"Michael",
+        age:100
+    }
+    , {
+        name:"George",
+        age:100
+    },
+     {
+        name:"Michelle",
+        age:100
+    }
+]
+
+console.log((arrayOfData[3]).name)
+// console.log(arrayOfData);""
+let age = 19;
+// let firstname;
+// let fullname;
+// let secondname ;
+
+// firstname= 120;
+// secondname = 120;
+// fullname = firstname + secondname +'';
+// console.log(typeof(false));
+// console.log(typeof(fullname));
+// console.log(typeof(1));
+
+let vote = "Good !! You are "+age+ " years old and Yes!!" + " You can vote";
+let notvote = "You cant vote go Home"
+// 1 .Tenary Operator
+console.log(age >= 18)
+age >= 18 ? console.log(vote) : console.log(notvote)
+
+// 2. Comparison Operator
+// == !=  ===  >= <=  >  < 
+// 3 .Logical Oprerator
+// 4. incremental and Decremental Operator
+// 5. Arithmetic Operator
+
+
+
+
+
+
+
+
+
+
+
+
+
