69da6e220d959d00b0cb716822636f7a00ae6b63
mrbassx <abassiddrisu9222@gmail.com>
2025-06-15 22:28:26 +0000

    up

---
+++ Software Weekends/js/MyScript.js
@@ -0,0 +1,46 @@
+// let CupOfGlass  = "Milky ";
+// let PlasticCup = "Orange ";
+// let SteelCup = "Juice";
+
+
+// let Cup = CupOfGlass +  PlasticCup + SteelCup ;
+// let z = Cup;
+// alert(z);
+// console.log(z);
+
+// document.getElementById("code").innerHTML = "I am Coding Out Here";
+// document.getElementById("code").innerHTML = "Milky Orange Juice "
+
+
+
+
+
+// Store Each Word And Print "I am Coding Out Here"
+// let Myi= "I ";
+// let MyAm = "am ";
+// let MyCoding = "Coding ";
+// let MyOut = "Out ";
+// let MyHere = "Here";
+
+// let All = Myi + MyAm + MyCoding + MyOut + MyHere  + 123;// Store Each Word And Print "I am Coding Out Here"
+
+// console.log(typeof(All));
+
+// alert(All);// Store Each Word And Print "I am Coding Out Here"
+
+// document.getElementById("code").innerHTML = All;
+
+// console.log(12222)
+
+let MyFirstNumber = 100;
+let MySecondNumber = 200;
+
+let Add = MyFirstNumber + MySecondNumber;
+let Mult = MyFirstNumber * MySecondNumber;
+let Div = MyFirstNumber / MySecondNumber;
+let Sub = MyFirstNumber - MySecondNumber;
+
+console.log(Add)
+console.log(Mult)
+console.log(Div)
+console.log(Sub)
