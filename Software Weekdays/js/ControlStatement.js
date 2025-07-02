e122af5161dd2ecef2650c731ba7739596750401
mrbassx <abassiddrisu9222@gmail.com>
2025-07-02 10:58:05 +0000

    up

---
+++ Software Weekdays/js/ControlStatement.js
@@ -0,0 +1,22 @@
+
+
+// 1 . Data @what the problem is about
+let age =  17;
+
+// 2. Logic @true Or false
+
+let cantVote = (age < 18) ; 
+
+// console.log("this is " + canVote)
+
+
+// 3 .check (control)
+if(cantVote){
+
+    console.log("You cant vote")
+
+}else{
+
+    console.log("You can vote")
+}
+    
