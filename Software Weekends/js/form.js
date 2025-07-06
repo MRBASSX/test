a94edeb24f2f735aa89755ce3e085cf0f250be9d
mrbassx <abassiddrisu9222@gmail.com>
2025-07-06 11:15:08 +0000

    updated...

--- Software Weekends/js/form.js
+++ Software Weekends/js/form.js
@@ -8,12 +8,29 @@ let form = document.getElementById("form")
 
 send.onclick = function (event) {
 
-
     event.preventDefault();
-
     const Data = new FormData(form)
     const FinalData = Object.fromEntries(Data.entries())
-    
     console.log(FinalData.firstname)
     console.log(FinalData.password)
+    console.log(FinalData.time)
+    console.log(FinalData.date)
+
+//    fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: "POST",
+//         body: JSON.stringify(FinalData),
+//         headers: {
+//             "Content-Type": "application/json"
+//         }
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         alert("Form submitted successfully!");
+//     })
+//     .catch(error => {
+//         console.error("Error:", error);
+//         alert("There was an error submitting the form.");
+//     });
+//     form.reset(); // Reset the form after submission
 }
