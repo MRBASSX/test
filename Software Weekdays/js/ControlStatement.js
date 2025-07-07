25a8f28dd7993b0b6c6157e73b2544cef9928290
mrbassx <abassiddrisu9222@gmail.com>
2025-07-07 11:19:06 +0000

    up

--- Software Weekdays/js/ControlStatement.js
+++ Software Weekdays/js/ControlStatement.js
@@ -39,26 +39,26 @@
     
 // }
 
-let amount = 1000
-let Totalpoint = 0
+// let amount = 1000
+// let Totalpoint = 0
 
-let peoples = [{
-    name:"Michael",
-    age:20
+// let peoples = [{
+//     name:"Michael",
+//     age:20
 
-},{
-    name:"Michile",
-    age:18
+// },{
+//     name:"Michile",
+//     age:18
 
-},{
-    name:"Mark",
-    age:21
+// },{
+//     name:"Mark",
+//     age:21
 
-},{
-    name:"George",
-    age:23
+// },{
+//     name:"George",
+//     age:23
 
-}]
+// }]
 // for (const person in peoples) {
 
 
@@ -76,42 +76,112 @@ let peoples = [{
     
 // }
 
-for (let person of peoples) {
+// for (let person of peoples) {
 
    
 
 
-    if (person.age ==18) {
-        person.Totalpoint = 1
+//     if (person.age ==18) {
+//         person.Totalpoint = 1
 
-    }else if(person.age ==20){
+//     }else if(person.age ==20){
 
-        person.Totalpoint = 2
+//         person.Totalpoint = 2
 
-    }else if(person.age ==23){
+//     }else if(person.age ==23){
 
-       person.Totalpoint = 3
+//        person.Totalpoint = 3
 
-    }else if(person.age ==21){
+//     }else if(person.age ==21){
 
-       person.Totalpoint = 4
+//        person.Totalpoint = 4
 
-    }
+//     }
     
-    Totalpoint += person.Totalpoint;
+//     Totalpoint += person.Totalpoint;
 
      
 
    
     
-}
+// }
 
 
-for (const person of peoples) {
+// for (const person of peoples) {
 
-    console.log(`'name: '${person.name} 'Share:' ${(person.Totalpoint/Totalpoint)*amount}`);
+//     console.log(`'name: '${person.name} 'Share:' ${(person.Totalpoint/Totalpoint)*amount}`);
+
+
+
+
     
+// }
+
+
+
+
+let firstnumber = parseInt(prompt("Input First Number"));
+let secondnumber = parseInt(prompt("Input Second Number"));
+let thirdnumber = parseInt(prompt("Input Third Number"));
+
+
+
+let test_firstnumber = firstnumber > secondnumber && firstnumber > thirdnumber;
+let test_secondnumber = secondnumber >firstnumber  && secondnumber > thirdnumber;
+let test_thirdnumber = thirdnumber > firstnumber && thirdnumber > secondnumber;
+
+
+
+if (test_firstnumber) {
+
+
+    alert("The first Number Is Bigger")
+    
+} else if(test_secondnumber){
+
+    alert("The Second Number Is Bigger")
+    
+} else if(test_thirdnumber){
+
+    alert("The Third number is Bigger")
+
+} else{
+
+    alert("Two numbers cant be the same")
 }
+
+
+
+
+
+
+let president = prompt("President Password")
+let vice = prompt("Vice Password");
+let speaker = prompt("Speakers Password")
+let justice = prompt("Justice Password")
+
+
+let DB_p = "123456789";
+let DB_v = "12345678910"
+let DB_s = "12345"
+let DB_j= "1234567"
+
+
+let test_president  = president == DB_p && vice == DB_v || president == DB_p && speaker == DB_s || president == DB_p && justice == DB_j 
+let test_vice  = vice == DB_v && president == DB_p || vice == DB_p && speaker == DB_s || vice == DB_p && justice == DB_j 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
