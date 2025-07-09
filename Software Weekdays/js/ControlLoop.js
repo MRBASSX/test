b9d05b4344e4b58627c97aeba265f99755cc3a59
mrbassx <abassiddrisu9222@gmail.com>
2025-07-09 11:23:53 +0000

    updated...

---
+++ Software Weekdays/js/ControlLoop.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+const Engineers = [
+    {
+        name:"Michelle",
+        age:100,
+        gender:"Female",
+        height:"10ft",
+
+        
+    },
+       {
+
+        name:"Michael",
+        age:100,
+        gender:"Male",
+        height:"10ft",
+        
+    },
+       {
+
+        name:"Mark",
+        age:100,
+        gender:"Male",
+        height:"10ft"
+        
+    },
+       {
+
+        name:"George",
+        age:100,
+        gender:"Male",
+        height:"10ft"
+        
+    },  
+]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+let  MyArray = [ 1, 2 ,3 ,4 , 5]
+
+console.log(MyArray)
+
+for(let upi = -100 /*Initial*/ ; upi <= -1 /*condition*/ ; upi = upi + 1 /* Update*/ ){
+
+    const element = upi;
+   console.log(element);/* Code To Execute*/
+
+}
