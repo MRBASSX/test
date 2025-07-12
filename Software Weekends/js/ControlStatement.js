f22d5f2562f43e0f81167bd6843d55a0b3b12b3d
mrbassx <abassiddrisu9222@gmail.com>
2025-07-12 08:56:45 -0400

    updated...

--- Software Weekends/js/ControlStatement.js
+++ Software Weekends/js/ControlStatement.js
@@ -1,6 +1,25 @@
 
+// let state  = document.getElementById("state");
 
+//  Take Data
+let Number1 = 123
+let Number2 = 140
 
+// Derived A Logic That Produce a true or false
+let Number1_IsBigger = Number1 > Number2;
+
+// Make A Check (control)
+if (Number1_IsBigger) {
+    
+// state.innerHTML = `<h1 style = "color:red;border:1px solid green"> The First Number: ${Number1} Is Bigger </h1>`;
+console.log("First Number Is The Biggest")
+
+}else{
+
+    console.log("Second Number Is The Biggest")
+// state.innerHTML = `<h1 style = "color:green; border:1px solid red">The Second Number: ${Number2} Is Bigger</h1>`;
+
+}
 
 
 
@@ -16,76 +35,78 @@
 
 // Control Statement Example~// This is a simple control statement example in JavaScript
 // It demonstrates the use of if statements
-let condition1 = true; // Change this to false to see the else block in action
+// let condition1 = true; // Change this to false to see the else block in action
 
-if (condition1) {
-    console.log("This is the if block again, condition is still true.");
-}
+// if (condition1) {
+//     console.log("This is the if block again, condition is still true.");
+// }
 
 
  
 
 // Control Statement Example// This is a simple control statement example in JavaScript
 // It demonstrates the use of if-else statements
-let condition = true; // Change this to false to see the else block in action
-if (condition) {
-    console.log("Condition is true, executing the if block.");
+    // let condition = true; // Change this to false to see the else block in action
+    // if (condition) {
+    //     console.log("Condition is true, executing the if block.");
+        
+    // } else {
+    //     console.log("Condition is false, executing the else block.");
     
-} else {
-    console.log("Condition is false, executing the else block.");
-  
-    
-    
-}
+        
+        
+    // }
 
 
 
 
-if (condition) {
-    
-} else if (!condition) {
-    console.log("This is the else if block, condition is false.");  
-    
-} else if(condition === null) {
+    // if (condition) {
+        
+    // } else if (!condition) {
+    //     console.log("This is the else if block, condition is false.");  
+        
+    // } else if(condition === null) {
 
-    console.log("This is the else if block, condition is null.");
-} else if (condition === undefined) {
-    console.log("This is the else if block, condition is undefined.");
-} else if (condition === 0) {
-    console.log("This is the else if block, condition is zero.");
-} else if (condition === "") {
-    console.log("This is the else if block, condition is an empty string.");
-} else if (condition === false) {
-    console.log("This is the else if block, condition is false.");
-} else if (condition === true) {            
-    console.log("This is the else block, condition is neither true nor false.");
-} else {
-    console.log("This is the final else block, condition is not met.");
-}
-
-
+    //     console.log("This is the else if block, condition is null.");
+    // } else if (condition === undefined) {
+    //     console.log("This is the else if block, condition is undefined.");
+    // } else if (condition === 0) {
+    //     console.log("This is the else if block, condition is zero.");
+    // } else if (condition === "") {
+    //     console.log("This is the else if block, condition is an empty string.");
+    // } else if (condition === false) {
+    //     console.log("This is the else if block, condition is false.");
+    // } else if (condition === true) {            
+    //     console.log("This is the else block, condition is neither true nor false.");
+    // } else {
+    //     console.log("This is the final else block, condition is not met.");
+    // }
 
 
 
-switch (condition) {
-    case true:
-        console.log("Switch case: Condition is true.");
+let MyAnswer = true
+
+switch (MyAnswer) {
+    case  1=="1":
+        console.log("Switch Is ONE");
         break;              
-    case false:
-        console.log("Switch case: Condition is false.");
+    case 2:
+        console.log("Switch Is TWo");
         break;
-    case null:
-        console.log("Switch case: Condition is null.");
+    case 3:
+        console.log("Switch Is Three");
         break;
-    case undefined:         
-        console.log("Switch case: Condition is undefined.");
+    case 4:         
+        console.log("Switch Is Four");
         break;
-    case Default:
+    default:
         console.log("Switch case: Default case executed.");
         break;
 
    
 }
+
+
 
 
 
