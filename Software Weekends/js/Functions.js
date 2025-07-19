7b190acfb684f647d3eb49fc47f17bf980522337
mrbassx <abassiddrisu9222@gmail.com>
2025-07-19 18:29:18 +0000

    up

--- Software Weekends/js/Functions.js
+++ Software Weekends/js/Functions.js
@@ -2,6 +2,49 @@
 
 // Type Of Functions
 
+// AnonyMous in
 
+// Arrow Function
+
+// Function With Name
+
+
+
+
+
+
+// Creating A Function
+
+let MyFunction = function (){
+
+
+    alert("HI ,I am Okay");
+
+
+}
+
+
+
+
+// Calling Function
+
+
+MyFunction();
+
+
+
+
+// Function with Parameter
+
+
+
+
+// Return  Function
+
+
+
+
+// CallBack Function
+ 
 
 
