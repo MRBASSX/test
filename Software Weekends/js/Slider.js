79f2ae737f4aafc8ed4a7563a6200df434e3e835
mrbassx <abassiddrisu9222@gmail.com>
2025-07-26 09:50:53 +0000

    updated...

--- Software Weekends/js/Slider.js
+++ Software Weekends/js/Slider.js
@@ -1,8 +1,25 @@
 
 
 
-function SlideShow(Test1) {
+function SlideShow() {
 
-    
+    let i ;
+    let slideindex = 0;
+
+    let slides = document.getElementsByClassName("slider-container")
+
+    if (slideindex > slides.length) {
+        slideindex = 1
+    }
+
+    for (let index = 0; index < slides.length; index++) {
+        const element = slides[index];
+
+        element.style.display = "none"
+        
+    }
+
+    slideindex++
+    slides[slideindex-]
     
 }
