642fb82a5fa9d74527bf2779a7bcc24365b3b8e9
mrbassx <abassiddrisu9222@gmail.com>
2025-08-05 11:38:55 +0000

    updated...

--- Software Weekdays/js/Arrays.js
+++ Software Weekdays/js/Arrays.js
@@ -70,7 +70,7 @@ function AddCart(productId) {
     let items = product.find( products => {return products.id ===productId} )
     if (localStorage.getItem("cart") == null) {
 
-    localStorage.setItem("cart",JSON.stringify(item))
+    localStorage.setItem("cart",JSON.stringify(items))
     
 } else{
 
