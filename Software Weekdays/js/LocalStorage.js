551f4d2ee4842aa32297cbd19e4efe51335fb8f3
mrbassx <abassiddrisu9222@gmail.com>
2025-08-12 11:18:15 +0000

    updateed....

--- Software Weekdays/js/LocalStorage.js
+++ Software Weekdays/js/LocalStorage.js
@@ -1,23 +1,28 @@
 
-const DataSet = [{
+const DataSet = [
+    {
+        name:"Michael",
+        age:120
 
-}];
-// JSON.stringify()
+}, {
+
+}, {
+
+}
+];
+let MyJsonString  = JSON.stringify(DataSet)
 // JSON.parse()
+console.log(MyJsonString)
 
-
-const DataSet2 = "I am Cool";
-
-
-
-
-alert("Hi")
-
-localStorage.setItem("key1",JSON.stringify(DataSet))
+localStorage.setItem("key1",MyJsonString)
 localStorage.setItem("key2"," I am Nice")
 localStorage.setItem("key3"," I am Good")
-
-
-let MyDataSet = JSON.parse(localStorage.getItem("key1"))
+localStorage.removeItem("key2")
+// localStorage.clear()
+let MyJsonArray = localStorage.getItem("key1")
+console.log(MyJsonString)
+let MyDataSet = JSON.parse(MyJsonArray)
 console.log(MyDataSet)
+
+
 
